3022f1f89944722488a46b4bd18b157341e1d8d2
Bhairav <bhairavgotam066@gmail.com>
2024-01-06 19:47:50 +0530

    added backend register and loging user routes

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
-const express = require("express")
+const express = require("express");
+const { UserRouter } = require("./routes/User.route");
+const { connection } = require("./config/db");
+
+
+const app = express();
+
+require("dotenv").config()
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+    res.send({"msg":"welcome to kryzen"})
+})
+
+app.use("/users",UserRouter)
+
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log({msg:"connected to DB"})
+    } catch (error) {
+        
+    }
+    console.log({msg:`server is running on port ${process.env.PORT}`})
+})
